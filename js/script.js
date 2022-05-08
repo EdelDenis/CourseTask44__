5a9bf2566e031d0d198169f46c27656304b9e3a6
EdelDenis <edeldenis12@gmail.com>
2022-05-08 10:38:25 +0000

    j

--- js/script.js
+++ js/script.js
@@ -28,42 +28,44 @@ const movieDB = {
 
 // Задание 1
 
-const adv = document.querySelectorAll(".promo__adv img");
+const adv = document.querySelectorAll(".promo__adv img") // выбрали селектор img картинок(псевдомассив)
 
 adv.forEach(item =>{
     item.remove();
-})
+});                   // - перебрали псевдомассив с картинками и удалили картинки
 
 // Вариант 2 
 
 //adv.forEach(function(item){
-  //  item.remove();
-//});
+   // item.remove();
+//})
 
 // Задание 2 
 
-const poster = document.querySelector(".promo__bg");
-const genre = poster.querySelector(".promo__genre");
+const poster = document.querySelector(".promo__bg") // - выделили селектор с постером(надо менять бекгдраунд)
+const genre = poster.querySelector(".promo__genre") // - выделили элемент с жанром (поменять жанр)
 
-genre.textContent = "Драма";
-
+genre.textContent = "Драма"; // - поменяли жанр на драму
 
 // Задание 3 
 
-poster.style.backgroundImage = "url('img/bg.jpg')";
+poster.style.backgroundImage = "url('img/bg.jpg')"; // - поменяли фон элементу (poster)
+
+
 
 // Задание 4
 
-const movieList = document.querySelector(".promo__interactive-list");
+const movieList = document.querySelector(".promo__interactive-list") // - создали переменную и выделили нужный селектор
 
-movieList.innerHTML = "";
+movieList.innerHTML = ""; - // очистили список фильмов
 
-movieDB.movies.sort();
-
-movieDB.movies.forEach((film,i) => {
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${i+1} ${film}
+movieDB.movies.forEach((film,i) =>{                       // - перебрали миссив обьекта movieDB -  список фильмов 
+    movieList.innerHTML+= `
+    <li class="promo__interactive-item">${i+1} ${film}   
        <div class="delete"></div>
     </li>
 `;
-});
+})  //  тем самым сделали список фильмов денамическим (берет данные из обьекта)
+    //  также добавили нумерацию фильмам массива из обьекта
+
+movieDB.movies.sort(); // - отсортировали по алфавиту массив 
